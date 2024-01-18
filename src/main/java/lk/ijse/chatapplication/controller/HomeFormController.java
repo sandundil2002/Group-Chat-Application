b5c50cc2642d5021bce20fc9ad68024fee0b71fe
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-01-18 18:54:27 +0530

    home page ui done...

--- src/main/java/lk/ijse/chatapplication/controller/HomeFormController.java
+++ src/main/java/lk/ijse/chatapplication/controller/HomeFormController.java
@@ -1,4 +1,11 @@
 package lk.ijse.chatapplication.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class HomeFormController {
+    public TextField txtName;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
 }
